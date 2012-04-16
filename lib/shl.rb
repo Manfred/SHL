607400297691985faef540e94603b2ff0f35baf6
Manfred Stienstra <manfred@fngtps.com>
2012-04-16 23:41:06 +0200

    Let's rely on the server for closing the socket for now.

--- lib/shl.rb
+++ lib/shl.rb
@@ -63,7 +63,7 @@ module SHL
     BL = 4096
     attr_accessor :io
     def parse
-      b='';while(d=io.read(BL));b<<d;end;io.close;b.split(N*2)
+      b='';while(d=io.read(BL));b<<d;end;b.split(N*2)
     end
     def body
       parse[1]
